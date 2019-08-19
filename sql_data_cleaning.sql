18fd603c16166f04c40ffaf9fd182dc6224ce5f5
Soroosh Rezazadeh <soroosh.rezazadeh@gmail.com>
2019-08-19 06:58:20 -0400

    SQL Data Cleaning

--- sql_data_cleaning.sql
+++ sql_data_cleaning.sql
@@ -110,3 +110,100 @@ SELECT first_name, last_name, CONCAT(first_name, '.', last_name, '@', name, '.co
        LENGTH(last_name) || 
        REPLACE(UPPER(name), ' ', '')) AS password
 FROM t1;
+
+/*
+Write a query to look at the top 10 rows to understand the columns and the raw data in the 
+dataset called sf_crime_data.
+*/
+SELECT *
+FROM sf_crime_data
+LIMIT 10;
+
+/*
+use the Quiz Question at the bottom of this page to make sure you remember the format that 
+dates should use in SQL.
+*/
+yyyy-mm-dd
+
+/*
+The format of the date column is mm/dd/yyyy with times that are not correct also at the 
+end of the date.
+Write a query to change the date into the correct SQL date format. You will need to use at 
+least SUBSTR and CONCAT to perform this operation
+*/
+SELECT date orig_date, (SUBSTR(date, 7, 4) || '-' || LEFT(date, 2) || '-' || SUBSTR(date, 4, 2)) new_date
+FROM sf_crime_data;
+
+/*
+Notice, this new date can be operated on using DATE_TRUNC and DATE_PART in the same way as 
+earlier lessons.
+*/
+SELECT date orig_date, (SUBSTR(date, 7, 4) || '-' || LEFT(date, 2) || '-' || SUBSTR(date, 4, 2))::DATE new_date
+FROM sf_crime_data;
+
+/*
+Run the query entered below in the SQL workspace to notice the row with missing data
+*/
+SELECT *
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id
+WHERE o.total IS NULL; 
+
+/*
+Use COALESCE to fill in the accounts.id column with the account.id for the NULL value
+*/
+SELECT COALESCE(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, o.*
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id
+WHERE o.total IS NULL;
+
+/*
+Use COALESCE to fill in the orders.account_id column with the account.id for the NULL value 
+*/
+SELECT COALESCE(a.id, a.id) filled_id, 
+       a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, 
+       COALESCE(o.account_id, a.id) account_id, o.occurred_at, o.standard_qty, 
+       o.gloss_qty, o.poster_qty, o.total, o.standard_amt_usd, o.gloss_amt_usd, 
+       o.poster_amt_usd, o.total_amt_usd
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id
+WHERE o.total IS NULL;
+
+/*
+Use COALESCE to fill in each of the qty and usd columns with 0 for the table
+*/
+SELECT COALESCE(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, COALESCE(o.account_id, a.id) account_id, o.occurred_at, COALESCE(o.standard_qty, 0) standard_qty, COALESCE(o.gloss_qty,0) gloss_qty, COALESCE(o.poster_qty,0) poster_qty, COALESCE(o.total,0) total, COALESCE(o.standard_amt_usd,0) standard_amt_usd, COALESCE(o.gloss_amt_usd,0) gloss_amt_usd, COALESCE(o.poster_amt_usd,0) poster_amt_usd, COALESCE(o.total_amt_usd,0) total_amt_usd
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id
+WHERE o.total IS NULL;
+
+/*
+Run the query in 1 with the WHERE removed and COUNT the number of ids
+*/
+SELECT COUNT(*)
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id;
+
+/*
+Run the before query, but with the COALESCE function used
+*/
+SELECT COALESCE(a.id, a.id) filled_id, 
+       a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, 
+       COALESCE(o.account_id, a.id) account_id, 
+       o.occurred_at, 
+       COALESCE(o.standard_qty, 0) standard_qty, 
+       COALESCE(o.gloss_qty,0) gloss_qty, 
+       COALESCE(o.poster_qty,0) poster_qty, 
+       COALESCE(o.total,0) total, 
+       COALESCE(o.standard_amt_usd,0) standard_amt_usd, 
+       COALESCE(o.gloss_amt_usd,0) gloss_amt_usd, 
+       COALESCE(o.poster_amt_usd,0) poster_amt_usd, 
+       COALESCE(o.total_amt_usd,0) total_amt_usd
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id;
